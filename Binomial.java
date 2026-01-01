9e6a0203bc92298a7bc00de263ee397e1e9cffc4
NoaSz <noa.szafran@post.runi.ac.il>
2026-01-01 21:38:13 +0200

    hw7 introCs

--- Binomial.java
+++ Binomial.java
@@ -13,51 +13,41 @@ public class Binomial {
 	// Computes the Binomial function, basic version.
 	public static int binomial1(int n, int k) { 
 		// Base cases
-		if (k > n)
-			return 0;
-		if (k == 0 || n == 0)
-			return 1;
+		      if (k > n) return 0L;
+        if (k == 0 || n == 0) return 1L;
+        return binomial1(n - 1, k) + binomial1(n - 1, k - 1);
+    }
 
-		// Recursive case
-		return binomial1(n - 1, k) + binomial1(n - 1, k - 1);
-	}
+    // Computes the Binomial function, efficiently (memoization)
+    public static long binomial(int n, int k) {
+        long[][] memo = new long[n + 1][k + 1];
 
-	
-	// Computes the Binomial function, efficiently
-	public static int binomial(int n, int k) {
-		// Create memo table
-		int[][] memo = new int[n + 1][k + 1];
+        // initialize all entries to -1
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= k; j++) {
+                memo[i][j] = -1L;
+            }
+        }
 
-		// Initialize memo with -1
-		for (int i = 0; i <= n; i++) {
-			for (int j = 0; j <= k; j++) {
-				memo[i][j] = -1;
-			}
-		}
+        return binomial(n, k, memo);
+    }
 
-		// Call recursive helper
-		return binomial(n, k, memo);
-	}
+    private static long binomial(int n, int k, long[][] memo) {
+        if (memo[n][k] != -1L) {
+            return memo[n][k];
+        }
 
-	private static int binomial(int n, int k, int[][] memo) {
-		if (memo[n][k] != -1) {
-			return memo[n][k];
-		}
-		// Base case
-		if ((k > n)) {
-			memo[n][k] = 0;
-			return 0;
-		}
-		// Another base case
-		if (n == 0 || k == 0) {
-			memo[n][k] = 1;
-			return 1;
-		}
-		memo[n][k] = binomial(n - 1, k, memo) + binomial(n - 1, k - 1, memo);
-		return memo[n][k];
-	}
+        if (k > n) {
+            memo[n][k] = 0L;
+            return 0L;
+        }
+
+        if (n == 0 || k == 0) {
+            memo[n][k] = 1L;
+            return 1L;
+        }
+
+        memo[n][k] = binomial(n - 1, k, memo) + binomial(n - 1, k - 1, memo);
+        return memo[n][k];
+    }
 }
-
-
-
-
--- IsSorted.java
+++ IsSorted.java
@@ -21,14 +21,11 @@ public class IsSorted {
         return isSorted(a, i + 1);
     }
  
-        return false; 
-    }
-
     public static void main(String[] args) {
-        int[] a = {1, 2, 2, 5};
+        int[] a = { 1, 2, 2, 5 };
         System.out.println(isSorted(a)); // true
 
-        int[] b = {1, 3, 2};
+        int[] b = { 1, 3, 2 };
         System.out.println(isSorted(b)); // false
     }
 }
